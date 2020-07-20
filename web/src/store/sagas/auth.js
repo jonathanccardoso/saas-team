0535684199e90c348d28ec86b95bbd074c3227b5
jonathanccardoso <jonathan99moura@hotmail.com>
2020-07-20 18:29:01 -0300

    Chore: update cors on server

--- web/src/store/sagas/auth.js
+++ web/src/store/sagas/auth.js
@@ -5,7 +5,7 @@ import AuthActions from "../ducks/auth";
 
 export function* signIn({ email, password }) {
   try {
-    const response = yield call(api.post, "session", { email, password });
+    const response = yield call(api.post, "sessions", { email, password });
 
     localStorage.setItem("@Omni:token", response.data.token);
 
