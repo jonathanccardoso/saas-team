834b322f5beca403b217551b0e3f78cbb5732149
jonathanccardoso <jonathan99moura@hotmail.com>
2020-07-23 17:41:34 -0300

    Fixme: finish form create project

--- web/src/components/Projects/index.js
+++ web/src/components/Projects/index.js
@@ -5,6 +5,7 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import ProjectsActions from "~/store/ducks/projects";
 
+import Modal from "~/components/Modal";
 import Button from "~/styles/components/Button";
 
 import { Container, Project } from "./styles";
@@ -12,9 +13,25 @@ import { Container, Project } from "./styles";
 class Projects extends Component {
   static propTypes = {
     getProjectsRequest: PropTypes.func.isRequired,
+    openProjectModal: PropTypes.func.isRequired,
+    closeProjectModal: PropTypes.func.isRequired,
+    createProjectRequest: PropTypes.func.isRequired,
     activeTeam: PropTypes.shape({
       name: PropTypes.string,
     }).isRequired,
+    projects: PropTypes.shape({
+      data: PropTypes.arrayOf(
+        PropTypes.shape({
+          id: PropTypes.number,
+          title: PropTypes.string,
+        })
+      ),
+      projectModalOpen: PropTypes.bool,
+    }).isRequired,
+  };
+
+  state = {
+    newProject: "",
   };
 
   componentDidMount() {
@@ -25,8 +42,28 @@ class Projects extends Component {
     }
   }
 
+  handleInputChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleCreateProject = (e) => {
+    e.preventDefault();
+
+    const { createProjectRequest } = this.props;
+    const { newProject } = this.state;
+
+    createProjectRequest(newProject);
+  };
+
   render() {
-    const { activeTeam, projects } = this.props;
+    const {
+      activeTeam,
+      projects,
+      openProjectModal,
+      closeProjectModal,
+    } = this.props;
+
+    const { newProject } = this.state;
 
     if (!activeTeam) return null;
 
@@ -35,7 +72,7 @@ class Projects extends Component {
         <header>
           <h1>{activeTeam.name}</h1>
           <div>
-            <Button onClick={() => {}}>+Novo</Button>
+            <Button onClick={openProjectModal}>+Novo</Button>
             <Button onClick={() => {}}>Membros</Button>
           </div>
         </header>
@@ -45,6 +82,28 @@ class Projects extends Component {
             <p>{project.title}</p>
           </Project>
         ))}
+
+        {projects.projectModalOpen && (
+          <Modal>
+            <h1>Criar projeto</h1>
+
+            <form onSubmit={this.handleCreateProject}>
+              <span>NOME</span>
+              <input
+                name="newProject"
+                value={newProject}
+                onChange={this.handleInputChange}
+              />
+
+              <Button size="big" type="submit">
+                Salvar
+              </Button>
+              <Button onClick={closeProjectModal} size="small" color="gray">
+                Cancelar
+              </Button>
+            </form>
+          </Modal>
+        )}
       </Container>
     );
   }
--- web/src/store/ducks/projects.js
+++ web/src/store/ducks/projects.js
@@ -5,6 +5,10 @@ import Immutable from "seamless-immutable";
 const { Types, Creators } = createActions({
   getProjectsRequest: null,
   getProjectsSuccess: ["data"],
+  openProjectModal: null,
+  closeProjectModal: null,
+  createProjectRequest: ["title"],
+  createProjectSuccess: ["project"],
 });
 
 export const ProjectsTypes = Types;
@@ -13,14 +17,24 @@ export default Creators;
 /* Initital State */
 export const INITIAL_STATE = Immutable({
   data: [],
+  projectModalOpen: false,
 });
 
 // Reducers
 
 export const success = (state, { data }) => state.merge({ data });
 
+export const openModal = (state) => state.merge({ projectModalOpen: true });
+export const closeModal = (state) => state.merge({ projectModalOpen: false });
+
+export const createSuccess = (state, { project }) =>
+  state.merge({ data: [...state.data, project] });
+
 // Reduces to types
 
 export const reducer = createReducer(INITIAL_STATE, {
   [Types.GET_PROJECTS_SUCCESS]: success,
+  [Types.OPEN_PROJECT_MODAL]: openModal,
+  [Types.CLOSE_PROJECT_MODAL]: closeModal,
+  [Types.CREATE_PROJECT_SUCCESS]: createSuccess,
 });
