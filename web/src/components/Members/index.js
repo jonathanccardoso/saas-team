43854e8b9f18bd96fff165ce03c71efaa39530d0
jonathanccardoso <jonathan99moura@hotmail.com>
2020-07-24 16:52:27 -0300

    Chore: finish view list members

--- web/src/components/Members/index.js
+++ web/src/components/Members/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 import { connect } from "react-redux";
@@ -11,29 +11,48 @@ import Button from "~/styles/components/Button";
 
 import { MembersList } from "./styles";
 
-const Members = ({ closeMembersModal }) => (
-  <Modal size="big">
-    <h1>Membros</h1>
+class Members extends Component {
+  static propTypes = {
+    closeMembersModal: PropTypes.func.isRequired,
+    getMembersRequest: PropTypes.func.isRequired,
+  };
 
-    <form>
-      <MembersList>
-        <li>
-          <strong>Diego Fernandes</strong>
-        </li>
-      </MembersList>
+  componentDidMount() {
+    const { getMembersRequest } = this.props;
 
-      <Button onClick={closeMembersModal} filled={false} color="gray">
-        Cancelar
-      </Button>
-    </form>
-  </Modal>
-);
+    getMembersRequest();
+  }
 
-Members.propTypes = {
-  closeMembersModal: PropTypes.func.isRequired,
-};
+  render() {
+    const { closeMembersModal, members } = this.props;
+
+    return (
+      <Modal size="big">
+        <h1>Membros</h1>
+
+        <form>
+          <MembersList>
+            {members.data.map((member) => (
+              <li key={member.id}>
+                <strong>{member.user.name}</strong>
+              </li>
+            ))}
+          </MembersList>
+
+          <Button onClick={closeMembersModal} filled={false} color="gray">
+            Cancelar
+          </Button>
+        </form>
+      </Modal>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  members: state.members,
+});
 
 const mapDispatchToProps = (dispatch) =>
   bindActionCreators(MembersActions, dispatch);
 
-export default connect(null, mapDispatchToProps)(Members);
+export default connect(mapStateToProps, mapDispatchToProps)(Members);
